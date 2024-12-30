0c86a1bd5cd651b142782324f31d49c202717857
thezeeshann <info.zeeshann@gmail.com>
2024-12-31 01:50:29 +0530

    updated

---
+++ src/lib/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colorBlack: "#27272a",
+  colorWhite: "#FFFFFF",
+  colorGray: "#242424",
+  colorGreen: "#CFF56A",
+  colorPink: "#FFD2D7",
+  colorBlue: "#A5BBD1",
+  colorOrange: "#FFC862",
+  colorViolet: "#C4B1D4",
+  colorMusic: "#DC148C",
+  colorPodcasts: "#006450",
+  colorLiveEvents: "#8400E7",
+};
