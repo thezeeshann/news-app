e9f272c4f0701bb49c54c9b84bdf227c458b5cd8
thezeeshann <info.zeeshann@gmail.com>
2025-01-11 22:45:50 +0530

    feat: added post api

---
+++ src/lib/api/post.ts
@@ -0,0 +1,22 @@
+import { BASE_URL } from "../contant";
+
+export const getPost = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/post/get`, {
+      method: "GET",
+      headers: {
+        "Content-type": "Application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result.data; // Extract only the posts data
+  } catch (error) {
+    console.error("Signup API error:", error);
+    throw error;
+  }
+};
