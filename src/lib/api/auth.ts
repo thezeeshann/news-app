7606acfc299254aee4f15415e7b4d7a59eb5f943
thezeeshann <info.zeeshann@gmail.com>
2025-01-12 21:23:20 +0530

    feat: added signin api

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,5 +1,6 @@
 import { BASE_URL } from "../contant";
 import { signupType } from "../schema/signup";
+import { signinType } from "../schema/signin";
 
 export const signup = async (data: signupType) => {
   try {
@@ -17,6 +18,30 @@ export const signup = async (data: signupType) => {
 
     const result = await response.json();
     return result;
+  } catch (error) {
+    console.error("Signup API error:", error);
+    throw error;
+  }
+};
+
+export const signin = async (data: signinType) => {
+  try {
+    const response = await fetch(`${BASE_URL}/auth/signin`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log("success message", response);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    return responseData;
   } catch (error) {
     console.error("Signup API error:", error);
     throw error;
