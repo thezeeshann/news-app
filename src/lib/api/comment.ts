2b6f7b7f074dd79042e29d459192f09a6692fc10
thezeeshann <info.zeeshann@gmail.com>
2025-01-22 01:14:26 +0530

    feat: Added create comment api

--- src/lib/api/comment.ts
+++ src/lib/api/comment.ts
@@ -1,4 +1,5 @@
 import { BASE_URL } from "../contant";
+import { commentType } from "../schema/comment";
 
 export const getComments = async () => {
   try {
@@ -7,6 +8,29 @@ export const getComments = async () => {
       headers: {
         "Content-type": "Application/json",
       },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result.data;
+  } catch (error) {
+    console.error("Signup API error:", error);
+    throw error;
+  }
+};
+
+export const createComment = async (data: commentType, token: string) => {
+  try {
+    const response = await fetch(`${BASE_URL}/comment/create`, {
+      method: "POST",
+      headers: {
+        "Content-type": "Application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
     });
 
     if (!response.ok) {
