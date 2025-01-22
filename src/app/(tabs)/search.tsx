2ec26c7aa817bb86b4d28eb5dac70d8b6cc4035c
thezeeshann <info.zeeshann@gmail.com>
2025-01-23 02:45:16 +0530

    refactor: update components

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,23 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, TextInput } from "react-native";
+import { View, TextInput, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import EvilIcons from "@expo/vector-icons/EvilIcons";
+import { useContext } from "react";
+import { AppContext } from "@/src/context/app-context";
+import { useDebounce } from "@/src/lib/hook";
+import { Link } from "expo-router";
 
 export default function Search() {
-  const [first, setfirst] = useState("");
+  const [results, setResults] = useState([]);
+  const [seachQuery, setSearchQuery] = useState("");
+  const { postData } = useContext(AppContext);
+  const debouncedValue = useDebounce(seachQuery);
+
+  useEffect(() => {
+    if (debouncedValue && postData) {
+      const filteredData = postData.filter((item) =>
+        item.title?.toLowerCase().includes(debouncedValue.toLowerCase()),
+      );
+      setResults(filteredData);
+    } else {
+      setResults([]);
+    }
+  }, [debouncedValue, postData]);
 
   return (
     <SafeAreaView className="bg-[#27272a] pt-0 px-4 h-full">
       <StatusBar style="light" />
-      <View className="flex flex-row items-center bg-[#333] p-1 rounded-lg mt-5">
+      <View className="flex flex-row items-center bg-[#333333] p-1 rounded-lg mt-5">
         <EvilIcons name="search" size={24} color="white" />
         <TextInput
+          value={seachQuery}
+          onChangeText={setSearchQuery}
           className="bg-[#333] text-white p-2 rounded-lg w-full"
           placeholder="Search News. Videos & Memes"
           placeholderTextColor="#888"
         />
       </View>
+      {results && (
+        <View
+          className={`${seachQuery ? "bg-[#333333]" : ""}  w-full p-2 mt-2 `}
+        >
+          {results.map((result) => (
+            <Link href={`/post/${result.id}`} key={result.id}>
+              <View className="flex flex-row items-center justify-between w-full">
+                <Text className="font-semibold text-white">{result.title}</Text>
+                <Image
+                  source={{ uri: result.image }}
+                  style={{ width: 40, height: 40 }}
+                />
+              </View>
+            </Link>
+          ))}
+        </View>
+      )}
     </SafeAreaView>
   );
 }
