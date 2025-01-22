66af78902b82ce5b213d57fc0a3ff03b9dbcef6d
thezeeshann <info.zeeshann@gmail.com>
2025-01-23 01:54:43 +0530

    refactor: update post schema

--- src/lib/schema/post.ts
+++ src/lib/schema/post.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const postSchema = z.object({
   title: z.string().min(5),
   content: z.string().optional(),
+  image: z.string().optional(),
 });
 
 export type postType = z.infer<typeof postSchema>;
